015a3466740e3517df20db337954f9d741ae27bd
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-25 15:28:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.py
@@ -0,0 +1,56 @@
+class Solution:
+    # Function to return a list of integers denoting spiral traversal of matrix.
+    def spirallyTraverse(self, matrix):
+        if not matrix:
+            return []
+
+        rows, cols = len(matrix), len(matrix[0])
+        top, bottom, left, right = 0, rows-1, 0, cols-1
+        result = []
+        
+        while len(result) < rows * cols:
+            for i in range(left, right+1):
+                result.append(matrix[top][i])
+            top += 1
+            
+            for i in range(top, bottom+1):
+                result.append(matrix[i][right])
+            right -= 1
+            
+            if top <= bottom:
+                for i in range(right, left-1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+            
+            if left <= right:
+                for i in range(bottom, top-1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+        
+        return result
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        r = int(data[index])
+        c = int(data[index + 1])
+        index += 2
+        matrix = []
+        for i in range(r):
+            row = list(map(int, data[index:index + c]))
+            matrix.append(row)
+            index += c
+
+        solution = Solution()
+        result = solution.spirallyTraverse(matrix)
+        print(" ".join(map(str, result)))
+
+# } Driver Code Ends
