a422431edf5c0abdb2d7d354526f90f6c9da338b
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-08-27 22:22:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def romanToDecimal(self, s): 
+        
+        roman_to_int = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+        
+        # Iterate over the Roman numeral string in reverse
+        for char in reversed(s):
+            value = roman_to_int[char]
+            
+            # If the current value is less than the previous value, subtract it
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            
+            # Update previous value for the next iteration
+            prev_value = value
+        
+        return total
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t = int(input())
+    for _ in range(t):
+        ob = Solution()
+        S = input()
+        print(ob.romanToDecimal(S))
+# } Driver Code Ends
