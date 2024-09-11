9f9640c9c4765c8ebace9bb18c14270427c60b7b
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-09-11 22:08:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.py
@@ -0,0 +1,59 @@
+#User function Template for python3
+
+'''
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def insertionSort(self, head):
+        temp = head
+        arr = []
+        while temp :
+            arr.append(temp.data)
+            temp = temp.next
+        arr.sort()
+        temp = head
+        for i in range(len(arr)):
+            temp.data = arr[i]
+            temp = temp.next
+        return head
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def printList(node):
+    while node:
+        print(node.data, end=" ")
+        node = node.next
+    print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+        head = None
+        if arr:
+            head = Node(arr[0])
+            tail = head
+            for value in arr[1:]:
+                tail.next = Node(value)
+                tail = tail.next
+
+        printList(Solution().insertionSort(head))
+
+# } Driver Code Ends
