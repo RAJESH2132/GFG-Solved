451abb46d6fdf38a719f4295d21d647079f64d4d
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-09-19 17:07:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Bit Difference/bit-difference.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    # Function to find number of bits needed to be flipped to convert A to B
+    def countBitsFlip(self,a,b):
+        ans = a ^ b
+        count = 0
+        for i in range(31):
+            if ans & 1<<i:
+                count += 1
+        return count
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+import math
+
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        ab=[int(x) for x in input().strip().split()]
+        a=ab[0]
+        b=ab[1]
+        ob=Solution()
+        print(ob.countBitsFlip(a,b))
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
