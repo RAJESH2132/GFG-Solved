722aae4b9130c66bf11df885a85d70f531ee235a
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-09-22 10:53:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.py
@@ -0,0 +1,34 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def print_divisors(self, n):
+        arr = []
+        i = 1
+        while i*i<=n:
+            if n%i==0:
+                arr.append(i)
+                if n/i != i:
+                    arr.append(n//i)
+            i+=1
+        arr.sort()
+        for i in arr:
+            print(i,end=' ')
+        
+        
+        
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        N = int(input())
+        ob = Solution()
+        ob.print_divisors(N)
+        print()
+# } Driver Code Ends
