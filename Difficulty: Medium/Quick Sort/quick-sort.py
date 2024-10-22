ebd1e2d919775d222b1d35b7dd240d63a948fcea
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-10-22 22:24:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.py
+++ Difficulty: Medium/Quick Sort/quick-sort.py
@@ -1,33 +1,39 @@
+
 #User function Template for python3
 
 class Solution:
     #Function to sort a list using quick sort algorithm.
-    def quickSort(self,arr,low,high):
-        # code here
-        if low < high:
-            
-            pi = self.partition(arr,low,high)
-            
-            self.quickSort(arr,low,pi-1)
-            self.quickSort(arr,pi+1,high)
-    
     def partition(self,arr,low,high):
         # code here
+        pivot = arr[high]
         i = low
         j = high-1
-        pivot = arr[high]
         
-        while i < j:
-            
+        while i<j:
             while i<high and arr[i]<pivot:
                 i+=1
             while j>low and arr[j]>=pivot:
                 j-=1
+            
             if i<j:
                 arr[i],arr[j] = arr[j],arr[i]
-        if arr[i] > pivot:
-            arr[i], arr[high] = arr[high],arr[i]
+        if arr[i]>pivot:
+            arr[i],arr[high] = arr[high],arr[i]
         return i
+    
+    def quickSort(self,arr,low,high):
+        # code here
+        if low < high:
+            pi = self.partition(arr,low,high)
+            self.quickSort(arr,low,pi-1)
+            self.quickSort(arr,pi+1,high)
+    
+        
+    
+
+
+
+
 
 #{ 
  # Driver Code Starts
