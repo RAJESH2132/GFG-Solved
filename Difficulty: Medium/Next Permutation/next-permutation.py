bd4639445e4232de21ffb14b1c5e653c523042af
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-11-19 14:51:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Permutation/next-permutation.py
+++ Difficulty: Medium/Next Permutation/next-permutation.py
@@ -1,7 +1,6 @@
-#User function Template for python3
-
 class Solution:
-    def nextPermutation(self, n, arr):
+    def nextPermutation(self,arr):
+        n = len(arr)
         ind = -1
         for i in range(n-2,-1,-1):
             if arr[i] < arr[i+1]:
@@ -9,16 +8,27 @@ class Solution:
                 break
         
         if ind == -1:
-            arr.reverse()
-            return arr
+            return self.rotated(arr,0,n)
+             
         
         for i in range(n-1,-1,-1):
             if arr[i] > arr[ind]:
                 arr[i], arr[ind] = arr[ind], arr[i]
                 break
+            
+        return self.rotated(arr,ind+1,n)
         
-        arr[ind+1:] = reversed(arr[ind+1:])
+    def rotated(self,arr,start,end):
+        left = start
+        right = end-1
+        while left<right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
         return arr
+            
+
+
 
 #{ 
  # Driver Code Starts
@@ -27,14 +37,15 @@ class Solution:
 if __name__ == '__main__':
     t = int(input())
     for _ in range(t):
-        N = int(input())
         arr = input().split()
+        N = len(arr)
         for i in range(N):
             arr[i] = int(arr[i])
-        
+
         ob = Solution()
-        ans = ob.nextPermutation(N, arr)
+        ob.nextPermutation(arr)
         for i in range(N):
-            print(ans[i],end=" ")
+            print(arr[i], end=" ")
         print()
+
 # } Driver Code Ends
