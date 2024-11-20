07f2977aeb93e3406dea6b541f434ac69f7aa424
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-11-20 21:41:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.py
+++ Difficulty: Easy/Bubble Sort/bubble-sort.py
@@ -1,17 +1,12 @@
 #User function Template for python3
 
 class Solution:
-    #Function to sort the array using bubble sort algorithm.
     def bubbleSort(self,arr):
         n = len(arr)
-        for i in range(n):
-            swapped = False
+        for i in range(n-1):
             for j in range(n-i-1):
                 if arr[j]>arr[j+1]:
                     arr[j], arr[j+1] = arr[j+1], arr[j]
-                    swapped = True
-            if swapped == "False":
-                break;
         return arr
         
 
