8c7cd626267469db24e9f46fbe41acabbb02d157
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-11-21 16:42:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.py
+++ Difficulty: Easy/Insertion Sort/insertion-sort.py
@@ -1,23 +1,20 @@
 #Sort the array using insertion sort
 
 class Solution:
-    # def insert(self, alist, index, n):
-        #code here
-        
-    #Function to sort the list using insertion sort algorithm.    
-    def insertionSort(self, arr, n):
-        #code here
-        for i in range(1,n):
-            key = arr[i]
-            j = i-1
+    def insertionSort(self, alist, n):
+        n = len(arr)
+
+        for i in range(1, n):  
+            key = arr[i]      
+            j = i - 1         
             
-            while j>=0 and key < arr[j]:
-                arr[j+1] = arr[j]
+            while j >= 0 and arr[j] > key:
+                arr[j + 1] = arr[j]
                 j -= 1
-            arr[j+1] = key
+            
+            arr[j + 1] = key
 
-
-
+        return arr
 
 
 #{ 
