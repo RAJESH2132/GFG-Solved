025820a3ad108016783eafa82ceb7904303e8def
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-11-29 12:28:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM And GCD/lcm-and-gcd.py
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.py
@@ -1,24 +1,14 @@
 #User function Template for python3
 
-def gcd(a: int, b: int) -> int:
-    # Euclidean algorithm to find GCD
-    while b != 0:
-        a, b = b, a % b
-    return a
 class Solution:
     def lcmAndGcd(self, a , b):
-        # code here 
-
-# def lcmAndGcd(a: int, b: int) -> list:
-    # Calculate GCD
-        gcd_ab = gcd(a, b)
+        lcm = abs(a*b)
+        while b!=0:
+            a,b = b,a%b
+        gcd = a
         
-        # Calculate LCM using the formula LCM(a, b) = abs(a * b) / GCD(a, b)
-        lcm_ab = abs(a * b) // gcd_ab
-    
-        return [lcm_ab, gcd_ab]
-
-
+        lcm//=gcd
+        return [lcm, gcd]
 
 
 #{ 
@@ -26,14 +16,15 @@ class Solution:
 #Initial Template for Python 3
 
 import math
-if __name__ == '__main__': 
-    t = int (input ())
-    for _ in range (t):
-        A,B=map(int,input().split())
-        
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        A, B = map(int, input().split())
+
         ob = Solution()
-        ptr = ob.lcmAndGcd(A,B)
-        
-        print(ptr[0],ptr[1])
+        ptr = ob.lcmAndGcd(A, B)
+
+        print(ptr[0], ptr[1])
         print("~")
+
 # } Driver Code Ends
