ef8550753cc2baed8a6273fc9ee33f9cf6657d50
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-12-02 14:43:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.py
+++ Difficulty: Easy/Selection Sort/selection-sort.py
@@ -1,30 +1,42 @@
 #User function Template for python3
 
 class Solution: 
-
-    def selectionSort(self, arr,n):
-        for i in range(n-1):
-            mini = i
-            for j in range(i+1,n):
-                if (arr[j]<arr[mini]):
-                    mini = j
-            arr[i] , arr[mini] = arr[mini] , arr[i]
+    def selectionSort(self, arr):
+        for i in range(len(arr)-1):
+            minIndex = i
+            for j in range(i+1,len(arr)):
+                if arr[j] < arr[minIndex]:
+                    minIndex = j
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
         return arr
-        
-
 
 #{ 
  # Driver Code Starts
-#Initial Template for Python 3
+class IntArray:
 
-if __name__ == '__main__': 
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  #array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
     t = int(input())
     for _ in range(t):
-        n = int(input())
-        arr = list(map(int, input().strip().split()))
-        Solution().selectionSort(arr, n)
-        for i in range(n):
-            print(arr[i],end=" ")
-        print()
+
+        arr = [int(i) for i in input().split()]
+
+        obj = Solution()
+        obj.selectionSort(arr)
+
+        IntArray().Print(arr)
         print("~")
+
 # } Driver Code Ends
