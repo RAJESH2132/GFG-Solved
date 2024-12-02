60b556097f7e25d9eb931b3a3ae862f461f4b4d2
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-12-02 16:17:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.py
+++ Difficulty: Easy/Insertion Sort/insertion-sort.py
@@ -1,36 +1,35 @@
-#Sort the array using insertion sort
-
-class Solution:
-    def insertionSort(self, alist, n):
-        n = len(arr)
-
-        for i in range(1, n):  
-            key = arr[i]      
-            j = i - 1         
-            
-            while j >= 0 and arr[j] > key:
-                arr[j + 1] = arr[j]
-                j -= 1
-            
-            arr[j + 1] = key
-
-        return arr
-
-
 #{ 
  # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# Please change the array in-place
+class Solution:
+    def insertionSort(self, arr):
+        for i in range(1,len(arr)):
+            key = arr[i]
+            j = i-1
+            while j>=0 and arr[j]>key:
+                arr[j+1] = arr[j]
+                j -= 1
+            arr[j+1] = key
+        return arr
+                
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
 if __name__ == "__main__":
     t = int(input())
-    for i in range(t):
-        n = int(input())
+    while t > 0:
         arr = list(map(int, input().split()))
-
-        Solution().insertionSort(arr, n)
-
-        for i in range(n):
-            print(arr[i], end=" ")
-
-        print()
+        ob = Solution()
+        ob.insertionSort(arr)
+        print(*arr)
         print("~")
+        t -= 1
+
 
 # } Driver Code Ends
