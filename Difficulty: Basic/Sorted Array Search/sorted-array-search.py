2adaf286a4e7c14d7f2613c41accb15ebbc81341
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-12-07 12:20:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sorted Array Search/sorted-array-search.py
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.py
@@ -1,32 +1,12 @@
 #User function Template for python3
 
 class Solution:
-    def searchInSorted(self, arr, K):
-        # Binary Search Algorithm
-        N = len(arr)
-        left, right = 0, N - 1
-        
-        while left <= right:
-            # Calculate the middle index
-            mid = left + (right - left) // 2
-            
-            # Check if the middle element is the target
-            if arr[mid] == K:
+    ##Complete this function
+    def searchInSorted(self,arr, k):
+        for i in range(len(arr)):
+            if arr[i] == k:
                 return True
-            # If the target is greater, ignore the left half
-            elif arr[mid] < K:
-                left = mid + 1
-            # If the target is smaller, ignore the right half
-            else:
-                right = mid - 1
-        
-        # Target not found
         return False
-
-
-
-
-
 
 #{ 
  # Driver Code Starts
