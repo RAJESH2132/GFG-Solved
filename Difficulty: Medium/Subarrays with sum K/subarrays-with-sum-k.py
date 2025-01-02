3b36d156a564793564349f3add7aa8f8cd08d326
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2025-01-02 12:27:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
@@ -1,36 +1,57 @@
 #User function Template for python3
 
 class Solution:
-    def findSubArraySum(self, arr, n, k):
-        # code here
-        dictt = {0:1}
-        preSum = 0
+    def countSubarrays(self, arr, k):
+        prefix_sum_count = {0: 1}  # Dictionary to store frequency of prefix sums
         count = 0
-        dictt[0] = 1
+        current_prefix_sum = 0
+    
         for num in arr:
-            preSum += num
-            remove = preSum-k
-            if remove in dictt:
-                count += dictt[remove]
-            if preSum in dictt:
-                dictt[preSum] += 1
+            current_prefix_sum += num
+            needed_prefix_sum = current_prefix_sum - k
+            
+            if needed_prefix_sum in prefix_sum_count:
+                count += prefix_sum_count[needed_prefix_sum]
+            
+            if current_prefix_sum in prefix_sum_count:
+                prefix_sum_count[current_prefix_sum] += 1
             else:
-                dictt[preSum] = 1
+                prefix_sum_count[current_prefix_sum] = 1
+    
         return count
+
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
+import atexit
+import io
+import sys
+from collections import deque
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
 if __name__ == '__main__':
-    t = int(input())
-    for _ in range (t):
-        N = int(input())
-        Arr = input().split()
-        for itr in range(N):
-            Arr[itr] = int(Arr[itr])
+    test_cases = int(input())
+    for cases in range(test_cases):
         k = int(input())
+        arr = list(map(int, input().strip().split()))
         ob = Solution()
-        print(ob.findSubArraySum(Arr, N, k))
+        res = ob.countSubarrays(arr, k)
+        print(res)
+        print("~")
+
 # } Driver Code Ends
